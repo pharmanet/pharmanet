d29eda7cd01dfc398c834b7e39cadb3e9de4002e
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2024-10-29 10:59:45 -0500

    first commit

---
+++ src/main/java/com/pharmanet/PharmanetApplication.java
@@ -0,0 +1,13 @@
+package com.pharmanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmanet/PharmanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
